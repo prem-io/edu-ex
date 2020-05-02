09bda117f14afdc313a5ef775fe1e54cd9b53206
prem-io <premk.lf@gmail.com>
2020-05-02 22:18:20 +0530

    style: ui fixes made

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -34,30 +34,36 @@ const Login = () => {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(state)
+  }
+
   return (
     <div className="row mx-0 auth-container">
       <div className="col-12 col-md-6 auth-form-container">
         <div className="ex-logo">EX</div>
         <div className="welcome-text">Welcome!</div>
         <small className="text-muted mb-4">Please Login to continue.</small>
-        <form className="form-container">
+        <form className="form-container" onSubmit={handleSubmit}>
           <div className="form-group">
             <label htmlFor="email">Email address</label>
-            <input type="email" className="form-control" value={state.email} onChange={handleInput} id="email" />
+            <input type="email" className="form-control" value={state.email} onChange={handleInput} id="email" required />
             <small id="emailHelp" className="form-text text-muted">{""}</small>
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <div className='password-field'>
-              <input type={state.showPassword ? "text" : "password"} className="form-control" value={state.password} onChange={handleInput} id="password" />
+              <input type={state.showPassword ? "text" : "password"} className="form-control" value={state.password} onChange={handleInput} id="password" required />
               {state.showPassword ?
                 <EyeFilled className="password-icon" onClick={() => handlePwdVisibility('password')} />
                 : <EyeInvisibleFilled className="password-icon" onClick={() => handlePwdVisibility('password')} />
               }
-              <small id="passwordHelp" className="form-text text-muted">min 8 characters, 1 special character</small>
+              <small className="form-text text-muted">min 8 characters, 1 special character</small>
+              <small className="form-text text-muted float-right cp">Forgot Password?</small>
             </div>
           </div>
-          <button type="submit" className="btn black-btn mt-2">Login</button>
+          <button type="submit" className="black-btn mt-2">Login</button>
         </form>
       </div>
       <div className="col-md-6 d-none d-md-block auth-img-container"></div>
